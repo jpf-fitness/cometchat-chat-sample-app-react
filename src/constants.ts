e731fcb6c9ef7d761f672208997dc353890bf516
samuvel <samuvel.johnson@learnship.com>
2023-10-04 21:37:44 +0530

    Add comet chat config

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // Head over to the Credentials section in your app on CometChat Dashboard and where you can find your unqiue App ID, Region and Auth Key. 😊
 
 export const CometChatConstants = {
-  appId: "APP_ID",
-  region: "REGION",
-  authKey: "AUTH_KEY",
+  appId: "2451446c06112cab",
+  region: "in",
+  authKey: "4a32d52918d469edd39450436b0e2d9293cb7308",
 };
