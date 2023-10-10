096b7a001ab1fc8da98f248148c6a228d8df5dce
samuvel <samuvel.johnson@learnship.com>
2023-10-10 10:12:56 +0530

    Change creds to prod

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // Head over to the Credentials section in your app on CometChat Dashboard and where you can find your unqiue App ID, Region and Auth Key. 😊
 
 export const CometChatConstants = {
-  appId: "2451446c06112cab",
-  region: "in",
-  authKey: "4a32d52918d469edd39450436b0e2d9293cb7308",
+  appId: "246488b5aa5dd701",
+  region: "us",
+  authKey: "689906cb472b0827a6bf2d272c023b08040e03f5",
 };
